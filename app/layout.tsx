cccede00ed1b434a834189460f1af4f5147cbafa
LuyandraBranco <luyandrabranco@gmail.com>
2025-04-14 20:27:17 +0100

    feat: favicon add

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,13 @@ import "./globals.css";
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
 });
 
 const volkhov = Volkhov({
   variable: "--font-volkhov",
   subsets: ["latin"],
-  weight: ["400", "700"],
+  weight: ["400", "700"]
 });
 
 export const metadata: Metadata = {
