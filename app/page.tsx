d66aa049673a4e40bc00d8af46b2362d6144f2a3
luyandra.branco <luyandrabranco@gmail.com>
2025-04-15 13:46:49 +0100

    refactor: fonts usadas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Header } from "@/components/Header/Header";
+import LandingPage from "./(landingPage)/page";
 
 export default function Home() {
   return (
-    <Header />
+    <LandingPage />
   )
 }
