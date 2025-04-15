1ea013aa31c76078f33dc5027906082f57efa7f3
whysocket <116583421+whysocket@users.noreply.github.com>
2025-04-15 10:09:30 +0100

    feat: use the expected navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Navbar from "@/components/Navbar";
+import { Header } from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <Navbar />
+    <Header />
   );
 }
