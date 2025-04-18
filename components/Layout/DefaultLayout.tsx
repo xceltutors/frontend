cf6309582a2f8b5424944168ce939a624630fe65
LuyandraBranco <luyandrabranco@gmail.com>
2025-04-18 15:52:52 +0100

    styles: bg changed to #f8f8f8

--- components/Layout/DefaultLayout.tsx
+++ components/Layout/DefaultLayout.tsx
@@ -6,7 +6,7 @@ import Footer from "../Footer/Footer";
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center space-y-[4rem] bg-[var(--custom-gray-200)]">
+    <div className="w-full min-h-screen flex flex-col items-center space-y-[4rem] bg-[#f8f8f8]">
       <Header />
       <main className="w-full flex flex-col space-y-[4rem]">{children}</main>
       <Footer />
