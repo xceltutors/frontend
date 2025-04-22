968f9e31ee185c7849711cc149d827ceb9de4d53
LuyandraBranco <luyandrabranco@gmail.com>
2025-04-22 21:44:42 +0100

    fix: elements don't used

--- app/prices/page.tsx
+++ app/prices/page.tsx
@@ -1,8 +1,6 @@
 import { InfoPrice } from "@/components/InfoPrice/InfoPrice";
 import DefaultLayout from "@/components/Layout/DefaultLayout";
-import { ArrowRight } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Prices() {
     return (
