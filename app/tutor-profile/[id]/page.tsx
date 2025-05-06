3601cc4fa94a686b77f3bb93fa553544697821c1
luyandra.branco <luyandrabranco@gmail.com>
2025-05-06 18:07:05 +0100

    ✨ feat: Experiences component

--- app/tutor-profile/[id]/page.tsx
+++ app/tutor-profile/[id]/page.tsx
@@ -1,18 +1,15 @@
-'use client';
 
 import Image from "next/image";
-import { ArrowLeft, CheckCircle, ChevronDown, ChevronUp, GraduationCap } from "lucide-react";
+import { ArrowLeft, CheckCircle, GraduationCap } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 import { SubjectTutor } from "@/app/tutor-profile/_components/subject-tutor";
 import { subjects } from "@/data/subjectsTutor";
 import TestimonialsSection from "../_components/testimonials";
 import { testimonials } from "@/data/testimonials";
+import { Experiences } from "../_components/experiences";
 
 export default function TutorProfile() {
-    const [showAll, setShowAll] = useState(false);
 
-    const toggleShow = () => setShowAll(!showAll);
     return (
         <div className="w-full min-h-screen">
             <div className="w-full px-10 sm:px-16 md:px-24 lg:px-42 xl:px-48 mx-auto">
@@ -116,55 +113,7 @@ export default function TutorProfile() {
                             Experiences
                         </h2>
 
-                        {showAll && (
-                            <>
-                                <div className="mb-6">
-                                    <div className="flex items-center mb-2">
-                                        <h3 className="font-(family-name:--font-volkhov) text-base md:text-xl font-bold">
-                                            Physics tutor
-                                        </h3>
-                                        <span className="mx-3 text-gray-400">—</span>
-                                        <span className="text-[var(--custom-gray-800)]">University of Cambridge (2022 - 2023)</span>
-                                    </div>
-                                    <ul className="list-disc pl-5 space-y-2 text-[var(--custom-gray-800)]">
-                                        <li>Experience teaching physics for college students</li>
-                                        <li>Lorem ipsum is simply dummy text</li>
-                                        <li>Lorem ipsum has been the industry's standard dummy text ever since the 1500s</li>
-                                        <li>Experience teaching physics for college students</li>
-                                        <li>Lorem ipsum is simply dummy text</li>
-                                        <li>Lorem ipsum has been the industry's standard dummy text ever since the 1500s</li>
-                                    </ul>
-                                </div>
-
-                                <div className="mb-6">
-                                    <div className="flex items-center mb-2">
-                                        <h3 className="font-(family-name:--font-volkhov) text-base md:text-xl font-bold">
-                                            Biology tutor
-                                        </h3>
-                                        <span className="mx-3 text-gray-400">—</span>
-                                        <span className="text-[var(--custom-gray-800)]">Primary Schools (2016 - 2018)</span>
-                                    </div>
-                                    <ul className="list-disc pl-5 space-y-2 text-[var(--custom-gray-800)]">
-                                        <li>Experience teaching physics for college students</li>
-                                        <li>Lorem ipsum is simply dummy text</li>
-                                        <li>Lorem ipsum has been the industry's standard dummy text ever since the 1500s</li>
-                                        <li>Experience teaching physics for college students</li>
-                                        <li>Lorem ipsum is simply dummy text</li>
-                                        <li>Lorem ipsum has been the industry's standard dummy text ever since the 1500s</li>
-                                    </ul>
-                                </div>
-                            </>
-                        )}
-
-                        <div className="flex justify-center border-t border-gray-200 pt-4">
-                            <button
-                                onClick={toggleShow}
-                                className="flex items-center text-[--custom-gray-800] hover:text-gray-900"
-                            >
-                                <span className="mr-1">{showAll ? 'View less' : 'View more'}</span>
-                                {showAll ? <ChevronUp className="w-4 h-4" /> : <ChevronDown className="w-4 h-4" />}
-                            </button>
-                        </div>
+                        <Experiences />
                     </div>
 
                     <section>
